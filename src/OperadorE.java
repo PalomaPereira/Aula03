8a093e1cc2d8f4e2f0b3406612d59fa6ec075400
Paloma Pereira <palomitas44@gmail.com>
2021-02-26 21:02:16 -0300

    Implementacao da Logica E

--- src/OperadorE.java
+++ src/OperadorE.java
@@ -3,18 +3,26 @@ import java.util.Scanner;
 public class OperadorE {
 
 	public static void main(String[] args) {
-		/*Faça um programa que verifique se o usuário digitado é igual a 'administrador' e se a senha digitada é igual a '123'*/
+		/*
+		 * Faça um programa que verifique se o usuário digitado é igual a
+		 * 'administrador' e se a senha digitada é igual a '123'
+		 */
 		Scanner leitor = new Scanner(System.in);
 		String usuario, senha;
 		System.out.println("SISTEMA DE LOGIN");
 		System.out.println("Por favor, digite seu username:");
 		usuario = leitor.next();
 		System.out.println("Por favor, digite sua senha:");
-		senha = leitor.next();	
-		//Aqui é preciso criar um if que verifique as duas condições para o login ser bem sucedido
-		
-		
-		
+		senha = leitor.next();
+		// Aqui é preciso criar um if que verifique as duas condições para o login ser
+		// bem sucedido
+		// equalsIgnoreCase: a comparação não fica case sensitive
+		if ((usuario.equalsIgnoreCase("administrador") && senha.equals("123"))) {
+			System.out.println("Usuário logado");
+		} else {
+			System.out.println("Dados incorretos!");
+		}
+
 		leitor.close();
 
 	}
